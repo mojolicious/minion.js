93ea618f0e9c0038147aa1cd601ff7bf4aa95a35
Sebastian Riedel <s@kraih.com>
2022-07-16 21:49:15 +0200

    Add documentation for Job class

--- src/job.ts
+++ src/job.ts
@@ -5,9 +5,21 @@ import type {JobInfo, MinionArgs, MinionJobId, RetryOptions} from './types.js';
  * Minion job class.
  */
 export class Job {
+  /**
+   * Arguments passed to task.
+   */
   args: MinionArgs;
+  /**
+   * Job id.
+   */
   id: MinionJobId;
+  /**
+   * Number of times job has been retried.
+   */
   retries: number;
+  /**
+   * Task name.
+   */
   task: string;
 
   _minion: Minion;
@@ -20,6 +32,10 @@ export class Job {
     this.task = task;
   }
 
+  /**
+   * Perform job in this process and return `null` if the task was successful or an exception otherwise. Note that
+   * this method should only be used to implement custom workers.
+   */
   async execute(): Promise<any | null> {
     try {
       const task = this.minion.tasks[this.task];
@@ -30,24 +46,41 @@ export class Job {
     }
   }
 
+  /**
+   * Transition from `active` to `failed` state with or without a result, and if there are attempts remaining,
+   * transition back to `inactive` with a delay based on `minion.backoff()`.
+   */
   async fail(result: any = 'Unknown error'): Promise<boolean> {
     if (result instanceof Error) result = {name: result.name, message: result.message, stack: result.stack};
     return await this.minion.backend.failJob(this.id, this.retries, result);
   }
 
+  /**
+   * Transition from `active` to `finished` state with or without a result.
+   */
   async finish(result?: any): Promise<boolean> {
     return await this.minion.backend.finishJob(this.id, this.retries, result);
   }
 
+  /**
+   * Get job information.
+   */
   async info(): Promise<JobInfo | null> {
     const info = (await this.minion.backend.listJobs(0, 1, {ids: [this.id]})).jobs[0];
     return info === null ? null : info;
   }
 
+  /**
+   * Change one or more metadata fields for this job. Setting a value to `null` will remove the field. The new values
+   * will get serialized as JSON.
+   */
   async note(merge: Record<string, any>): Promise<boolean> {
     return await this.minion.backend.note(this.id, merge);
   }
 
+  /**
+   * Return all jobs this job depends on.
+   */
   async parents(): Promise<Job[]> {
     const results: Job[] = [];
 
@@ -63,6 +96,9 @@ export class Job {
     return results;
   }
 
+  /**
+   * Perform job and wait for it to finish. Note that this method should only be used to implement custom workers.
+   */
   async perform(): Promise<void> {
     const error = await this.execute();
     if (error === null) {
@@ -72,14 +108,23 @@ export class Job {
     }
   }
 
+  /**
+   * Remove `failed`, `finished` or `inactive` job from queue.
+   */
   async remove(): Promise<boolean> {
     return await this.minion.backend.removeJob(this.id);
   }
 
+  /**
+   * Transition job back to `inactive` state, already `inactive` jobs may also be retried to change options.
+   */
   async retry(options: RetryOptions) {
     return await this.minion.backend.retryJob(this.id, this.retries, options);
   }
 
+  /**
+   * Minion instance this job belongs to.
+   */
   get minion(): Minion {
     return this._minion;
   }
